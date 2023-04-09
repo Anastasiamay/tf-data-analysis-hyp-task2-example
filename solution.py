b30a904adffbdce73c95da02f151406d1246f183
Anastasia <112168910+Anastasiamay@users.noreply.github.com>
2023-04-09 13:39:53 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ks_2samp
+from scipy.stats import cramervonmises_2samp
 
 chat_id = 1067114867 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    _, p_value = ks_2samp(x, y)
+    _, p_value = cramervonmises_2samp(x, y)
     alpha = 0.03
     return p_value < alpha
 
