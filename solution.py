da777158ddc0a4374db761e5f3f34d3f66f11917
Anastasia <112168910+Anastasiamay@users.noreply.github.com>
2023-04-09 14:02:01 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,8 @@ from scipy.stats import cramervonmises_2samp
 chat_id = 1067114867 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    _, p_value = cramervonmises_2samp(x, y)
+    result = cramervonmises_2samp(x, y)
+    p_value = result.pvalue
     alpha = 0.03
     return p_value < alpha
 
